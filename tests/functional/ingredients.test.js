1e5d63d9787ac8666dddf4ca2e4dfb9a600ea28d
nishi-moto <alessandra@nishimoto.me>
2020-08-21 08:41:39 +0200

    added functional testing

---
+++ tests/functional/ingredients.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const app = require('../testServer');
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(3002);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+describe('Ingredients endpoint', () => {
+  it('should return 200', async () => {
+    const res = await request(server)
+      .get('/food/ingredients/autocomplete?ingredients=apple')
+      .send();
+    expect(res.statusCode).toEqual(200);
+  });
+});
